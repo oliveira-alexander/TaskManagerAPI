282b56abb12a1bfc518503b92b5a76c113604114
oliveira-alexander <oliveir4.4lexander@gmail.com>
2025-05-06 08:53:40 -0300

    Criados DBContext e pastas do sistema

---
+++ TaskManagerAPI/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+using TaskManagerAPI.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+// DB Context
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseInMemoryDatabase("TaskManagerDB"));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapScalarApiReference();
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
